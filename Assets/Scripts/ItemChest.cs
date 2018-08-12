ec3ee9ba44aabdd4243df4dbcbc421ad0bfb17a5
Brandon Smith <brandon.smith.945@gmail.com>
2018-08-12 13:55:21 -0500

    Progress on items and enemies

---
+++ Assets/Scripts/ItemChest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemChest : MonoBehaviour {
+
+	public Sprite openedSprite;
+	public GameObject[] backpackItemPrefabs;
+	private int containedItemIndex;
+
+	public GameObject ContainedItemPrefab {
+		get {
+			return this.backpackItemPrefabs[this.containedItemIndex];
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		this.containedItemIndex = Mathf.FloorToInt(Random.Range(0, backpackItemPrefabs.Length));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log("Triggered chest");
+		if(other.name == "hero") {
+			BackpackController backpack = FindObjectOfType<BackpackController>();
+			//backpack.ReceiveItemFromHero(Instantiate(this.ContainedItemPrefab));
+			this.GetComponent<SpriteRenderer>().sprite = this.openedSprite;
+		}
+	}
+}
