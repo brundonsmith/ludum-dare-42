f3773b45923f6d958b2ab49222f93721d9564e22
Brandon Smith <brandon.smith.945@gmail.com>
2018-08-12 13:55:21 -0500

    Progress on items and enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour {
+
+	public readonly static int ManaPerAttack = 3;
+	public readonly static int StaminaPerAttack = 1;
+
+	public int health;
+	public int damage;
+
+	private ScrollManager scrollManager;
+	private BackpackController backpackController;
+	private bool battling = false;
+
+	// Use this for initialization
+	void Start () {
+		this.scrollManager = FindObjectOfType<ScrollManager>();
+		this.backpackController = FindObjectOfType<BackpackController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.name == "hero") {
+			scrollManager.Pause();
+			this.battling = true;
+
+			/* if(has enough mana) */
+			// trigger spell animation
+			
+		}
+	}
+}
--- Assets/Scripts/ScrollManager.cs
+++ Assets/Scripts/ScrollManager.cs
@@ -12,48 +12,78 @@ using UnityEngine;
 
 public class ScrollManager : MonoBehaviour {
 
-	public GameObject[] dirtstripPrefabs;
 	public float scrollSpeed;
+	public float enemyChance;
+	public float chestChance;
+
+	public GameObject[] dirtstripPrefabs;
+	public GameObject[] enemyPrefabs;
+	public GameObject chestPrefab;
 
 	private GameObject dirt_container;
+	private bool paused = false;
 
 	// Use this for initialization
 	void Start () {
 		dirt_container = GameObject.Find("dirt_container");
 	}
 
+	// Update is called once per frame
+	void Update () {
+		if(!paused) {
+			
+			// move everything along and possible create/delete strips
+			Transform[] dirt_strips = dirt_container.GetComponentsInChildren<Transform>();
+			foreach (Transform dirt_strip in dirt_strips) {
+				if(dirt_strip.transform.parent == dirt_container.transform) {
+					if (dirt_strip.position.x < -10) {
+
+						//get starting placement of dirt_strip
+						float startingPosition = dirt_strip.position.x;
+						float positionChange = startingPosition + 21 - getMovement();
+
+						// destroy last dirt strip
+						GameObject.Destroy(dirt_strip.gameObject);
+
+						// randomly pick a dirt_strip from the array and create it at start of screen
+						createDirtStrip(positionChange);
+
+					} else {
+						dirt_strip.position = dirt_strip.position - new Vector3(getMovement(), 0, 0);
+					}
+				}
+			}
+
+			// possibly spawn enemy
+			if(Random.value < this.enemyChance) {
+				Instantiate(randomElement(enemyPrefabs), new Vector3(22, 0, 0), Quaternion.identity, dirt_container.transform);
+			}
+
+			// possibly spawn chest
+			if(Random.value < this.chestChance) {
+				Instantiate(chestPrefab, new Vector3(22, 0, 0), Quaternion.identity, dirt_container.transform);
+			}
+		}
+	}
+
 	void createDirtStrip(float positionChange){
-		float randomNum = Random.Range(0, dirtstripPrefabs.Length);
-		int index = Mathf.FloorToInt(randomNum);
-		GameObject new_dirt_strip = GameObject.Instantiate(dirtstripPrefabs[index], new Vector3(positionChange, 0, 0), Quaternion.identity);
+		GameObject new_dirt_strip = GameObject.Instantiate(randomElement(dirtstripPrefabs), new Vector3(positionChange, 0, 0), Quaternion.identity);
 		new_dirt_strip.transform.parent = dirt_container.transform;
 	}
 
-	// Update is called once per frame
-	void Update () {
-		float speedChange = Time.deltaTime * scrollSpeed;
+	public void Pause() {
+		this.paused = true;
+	}
 
-		Transform[] dirt_strips = dirt_container.GetComponentsInChildren<Transform>();
+	public void Resume() {
+		this.paused = false;
+	}
 
-		foreach (Transform dirt_strip in dirt_strips) {
-			if(dirt_strip.transform.parent == dirt_container.transform) {
-				if (dirt_strip.position.x < -10) {
+	private float getMovement() {
+		return Time.deltaTime * scrollSpeed;
+	}
 
-					//get starting placement of dirt_strip
-					float startingPosition = dirt_strip.position.x;
-					float positionChange = startingPosition + 21 - speedChange;
-
-					// destroy last dirt strip
-					GameObject.Destroy(dirt_strip.gameObject);
-
-					// randomly pick a dirt_strip from the array and create it at start of screen
-					createDirtStrip(positionChange);
-
-				} else {
-					dirt_strip.position = dirt_strip.position - new Vector3(speedChange, 0, 0);
-				}
-			}
-		}
-
+	private GameObject randomElement(GameObject[] arr) {
+		return arr[Mathf.FloorToInt(Random.Range(0, arr.Length))];
 	}
 }
